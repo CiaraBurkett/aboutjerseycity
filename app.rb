5225f7b4d09b6b90814e083517b34a6449461593
Ciara Burkett <ciaraburkett@gmail.com>
2013-05-01 23:43:19 -0400

    Universal layout

--- app.rb
+++ app.rb
@@ -4,3 +4,18 @@ require 'slim'
 get '/' do
   slim :index
 end
+
+__END__
+@@layout
+doctype html
+html
+  head
+    meta charset="utf-8"
+    title About Jersey City, a local directory
+
+  body
+  	.container
+  		==yield
+
+  		footer
+  			p This is the footer.
