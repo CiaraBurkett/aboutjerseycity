854b77fa12a7dc4c98b9ca764f69dd660998e7d7
Ciara Burkett <ciaraburkett@gmail.com>
2013-05-06 08:08:46 -0400

    Create rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aboutjerseycity::Application.config.secret_token = '73a1a9143382af4e716afbbd0a5c217c9bf1c02fc8294c35cf31c5915d9311f12ba28a50406898188ea9853964492989bc0d546484070f69ca708cf1cc99ae49'
