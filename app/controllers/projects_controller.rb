74f54ae27c83fe703125a8d2a873ff72f1aecc38
Ciara Burkett <ciaraburkett@gmail.com>
2013-06-02 09:05:18 -0400

    Allow users who are not signed in to view individual project pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_filter :authenticate_user!, except: [:index]
+  before_filter :authenticate_user!, except: [:show, :index]
 
   # GET /projects
   # GET /projects.json
