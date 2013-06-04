c8eb261ec66502794ba25d442cba3c99df825955
Ciara Burkett <ciaraburkett@gmail.com>
2013-06-04 09:03:06 -0400

    Troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,5 +69,5 @@ Aboutjerseycity::Application.configure do
   GA.tracker = "UA-41456421-1"
 
   # Default Devise URL for production environment
-  config.action_mailer.default_url_options = { :host => 'aboutjerseycity.com' }
+  config.action_mailer.default_url_options = { :host => 'aboutjerseycity.herokuapp.com' }
 end
